642f06ff1bb5d3846c8f547cdbc8a54363f40dea
cpdnagumo <cpdnagumo@users.noreply.github.com>
2026-09-09 11:34:50 -0300

    Configurar conexao com Supabase Free

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.FLV_SUPABASE_CONFIG = {
-  url: "COLE_A_URL_DO_SEU_PROJETO_SUPABASE",
-  anonKey: "COLE_A_CHAVE_ANON_PUBLICA_DO_SEU_PROJETO"
+  url: "https://flvbhogcykuqirnorpmp.supabase.co",
+  anonKey: "sb_publishable_uyZ4IsZ9uigZtpfKiQLdvw_KcY0mMXe"
 };
