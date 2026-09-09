2757a3da9cba1e9381d456f2d571e7300edb7f0f
cpdnagumoCd <iasmin.hernandez12@gmail.com>
2026-09-09 14:20:05 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.FLV_SUPABASE_CONFIG = {
   url: "https://flvbhogcykuqirnorpmp.supabase.co",
-  anonKey: "sb_publishable_uyZ4IsZ9uigZtpfKiQLdvw_KcY0mMXe"
+  anonKey: "https://flvbhogcykuqirnorpmp.supabase.co/rest/v1/"
 };
