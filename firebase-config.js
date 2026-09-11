ae7f429886b0302cc43014d5cd614e02f7eade7a
cpdnagumo <cpdnagumo@users.noreply.github.com>
2026-09-11 14:44:37 -0300

    Corrigir formato do firebase-config

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.FLV_FIREBASE_CONFIG = {
   apiKey: "AIzaSyAKz3L3cvbzrLWGb3avdUH2zWxGFM8ueLY",
-  authDomain: "controleflv-6ff35.firebaseapp.com",
-  projectId: "controleflv-6ff35",
-  storageBucket: "controleflv-6ff35.firebasestorage.app",
+  authDomain: "controlflv-6ff35.firebaseapp.com",
+  projectId: "controlflv-6ff35",
+  storageBucket: "controlflv-6ff35.firebasestorage.app",
   messagingSenderId: "560427118098",
-  appId: "1:560427118098:web:bf5eab74cbdc9d81c2f2cb",
-  measurementId: "G-WHET34698F"
-
+  appId: "1:560427118098:web:bf5eab74cbdc9d81c2f2cb"
 };
-
