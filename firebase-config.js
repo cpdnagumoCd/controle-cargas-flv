8d3fdfedeb45faff593e751e92e1698566c298b4
cpdnagumoCd <iasmin.hernandez12@gmail.com>
2026-09-11 17:00:08 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.FLV_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAKz3L3cvbzrLWGb3avdUH2zWxGFM8ueLY",
-  authDomain: "controlflv-6ff35.firebaseapp.com",
-  projectId: "controlflv-6ff35",
-  storageBucket: "controlflv-6ff35.firebasestorage.app",
-  messagingSenderId: "560427118098",
-  appId: "1:560427118098:web:bf5eab74cbdc9d81c2f2cb"
-};
