bd8d44d32005fe31cf5f85b36e4f1a2ee6599cef
cpdnagumoCd <iasmin.hernandez12@gmail.com>
2026-09-11 10:11:21 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
- apiKey: "AIzaSyBSI8ADRglol_0pUCKYXeLyARDpkDJqDTk",
+window.FLV_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBSI8ADRglol_0pUCKYXeLyARDpkDJqDTk",
   authDomain: "controle-flv-740df.firebaseapp.com",
   projectId: "controle-flv-740df",
   storageBucket: "controle-flv-740df.firebasestorage.app",
   messagingSenderId: "654170136545",
-  appId: "1:654170136545:web:ff37d4910484ca966a7ca0",
-  measurementId: "G-9FS8RM8ZM6"
+  appId: "1:654170136545:web:ff37d4910484ca966a7ca0"
 };
